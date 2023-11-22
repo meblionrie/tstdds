ba74b945857ab965aad22be5a644d7bee487796c
Anurag Kumar <129645919+meblionrie@users.noreply.github.com>
2023-11-22 18:40:11 +0530

    Update go.py

--- go.py
+++ go.py
@@ -1,6 +1,6 @@
-
 import requests
 from concurrent.futures import ThreadPoolExecutor
+import time
 
 def send_request(url):
     try:
@@ -11,16 +11,21 @@ def send_request(url):
         print(f"Error sending request to {url}: {str(e)}")
 
 if __name__ == "__main__":
-    # Replace 'http://example.com' with your server's URL
-    base_url = 'http://example.com'
+    # Get user input for testing parameters
+    base_url = input("Enter the website URL to test: ")
+    num_threads = int(input("Enter the number of threads: "))
+    num_requests = int(input("Enter the number of requests: "))
+    test_duration = int(input("Enter the time duration for testing (in seconds): "))
     
-    # Number of requests to send
-    num_requests = 1000000
-    
-    # Adjust the number of threads based on your system capabilities
-    num_threads = 50
-
+    # Create a list of URLs based on user input
     urls = [base_url] * num_requests
+    
+    # Measure the start time
+    start_time = time.time()
 
     with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        executor.map(send_request, urls)
+        # Run until either the specified duration is reached or all requests are sent
+        while (time.time() - start_time) < test_duration:
+            executor.map(send_request, urls)
+
+    print(f"Testing completed in {time.time() - start_time:.2f} seconds.")
